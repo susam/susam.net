6d74c9a33056ca21ef8b98a7b2f8a79d2082db12
Susam Pal <susam@susam.in>
2020-02-15 13:03:31 +0530

    Upgrade from MathJax 2.7.4 to MathJax 3

--- static/js/tex.js
+++ static/js/tex.js
@@ -1,25 +1,3 @@
-// MathJax configuration.
-window.MathJax = {
-  tex2jax: {
-    // Enable $...$ as delimiter for inline math.
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    processEscapes: true
-  },
-
-  TeX: {
-    // Enable equation numbering.
-    equationNumbers: {
-      autoNumber: 'AMS'
-    },
-
-    Macros: {
-      qed: '\\tag*{\\( \\blacksquare \\)}',
-      bitxor: '\\operatorname{\\hat{}}',
-      ord: '\\operatorname{ord}',
-    }
-  }
-}
-
 ;(function () {
   function loadjs(url) {
       var script = window.document.createElement('script')
@@ -27,6 +5,5 @@ window.MathJax = {
       window.document.head.appendChild(script)
   }
 
-  loadjs('https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/' +
-         'MathJax.js?config=TeX-AMS_CHTML')
+  loadjs('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js')
 })()
