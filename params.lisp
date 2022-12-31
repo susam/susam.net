e9e739f7e1d81738a3cfc4fad86de2f4ccf1e3e5
Susam Pal <susam@susam.net>
2022-12-31 13:39:13 +0000

    Add parameters file to populate layout

---
+++ params.lisp
@@ -0,0 +1,7 @@
+;;; Copyright (c) 2022 Susam Pal
+;;; All rights reserved.
+
+(("author" . "Susam Pal")
+ ("subtitle" . " - Susam Pal")
+ ("site-url" . "https://susam.net/")
+ ("initial-year" . 2001))
