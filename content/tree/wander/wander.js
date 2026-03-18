39ae5930a49d8be5c70553812b845a675b34b6b3
Susam Pal <susam@susam.net>
2026-03-18 23:59:21 +0000

    Add Wander version 0.1.0

---
+++ content/tree/wander/wander.js
@@ -0,0 +1,31 @@
+window.wander = {
+  // Other Wander consoles in my neighbourhood.
+  consoles: [
+    'https://susam.net/wander/',
+    'https://dahlstrand.net/wander/',
+    'https://www.siddharthagolu.com/wander/',
+    'https://wander.liontask.dev/',
+    'https://juleskourelakos.com/wander/',
+    'https://bitzero.cloud/wander/',
+  ],
+  // My recommendations.
+  pages: [
+    'http://www.antonis.de/qbebooks/gwbasman/',
+    'https://aartaka.me/',
+    'https://int10h.org/',
+    'https://int10h.org/oldschool-pc-fonts/fontlist/',
+    'https://maxwellito.com/',
+    'https://midnight.pub/',
+    'https://sachachua.com/',
+    'https://susam.net/',
+    'https://thejeshgn.com/',
+    'https://www.bitsavers.org/',
+    'https://www.norvig.com/',
+    'https://chrismorgan.info/',
+    'https://quarters.captaintouch.com/blog/',
+    'https://www.evalapply.org/',
+    'https://jgc.org/',
+    'https://xnacly.me/',
+    'https://tonsky.me/',
+  ],
+}
